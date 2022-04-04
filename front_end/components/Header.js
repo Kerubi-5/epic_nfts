392f712cbcfde63388f67cb6cffe00b95d406664
John Kim A. Querobines <querobinesj@gmail.com>
2022-04-04 19:52:05 +0800

    style: header

--- front_end/components/Header.js
+++ front_end/components/Header.js
@@ -1,12 +1,14 @@
 const Header = ({ connect, currentAccount }) => {
   return (
-    <header className="flex justify-between">
-      <div>Logo</div>
-      <div>
+    <header className="flex justify-between items-center bg-slate-800 shadow-md">
+      <div className="text-2xl font-extrabold px-2 text-transparent bg-clip-text gradient">
+        KK
+      </div>
+      <div className="bg-gray-500 py-2 px-4 rounded-md m-1 text-white font-semibold">
         {currentAccount === "" ? (
           <button onClick={connect}>Connect</button>
         ) : (
-          currentAccount
+          <div>{currentAccount}</div>
         )}
       </div>
     </header>
