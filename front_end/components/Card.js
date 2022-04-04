1c532d66da779549058edd64baa815e9d84d8bdb
John Kim A. Querobines <querobinesj@gmail.com>
2022-04-04 19:13:51 +0800

    style: adds hover animation on cards

--- front_end/components/Card.js
+++ front_end/components/Card.js
@@ -1,6 +1,6 @@
 const Card = () => {
   return (
-    <div className="bg-white p-4 rounded-lg shadow-lg">
+    <div className="bg-white p-4 rounded-lg shadow-lg hover:shadow-2xl hover:scale-105 transition-all duration-300 cursor-pointer">
       <div className="h-96 w-96">Picture here</div>
       <div>
         <h3>Title</h3>
